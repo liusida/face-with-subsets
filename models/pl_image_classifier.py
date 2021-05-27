98ce2eaf9af3389c8f7497a661036a6092ffc46a
sida <sliu1@uvm.edu>
2021-05-27 11:34:24 -0400

    Auto Save

--- models/pl_image_classifier.py
+++ models/pl_image_classifier.py
@@ -7,30 +7,44 @@ import pytorch_lightning as pl
 
 class LitImageClassifier(pl.LightningModule):
 
-    def __init__(self, input_dim=196608, output_dim=10):
+    def __init__(self, output_dim=10): # input_dim = [3, 256, 256]
         super().__init__()
-        self.flatten = nn.Flatten()
-        self.fc1 = nn.Linear(input_dim, output_dim)
+        self.conv1 = nn.Conv2d(3, 6, 8)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(6, 16, 8)
+        self.conv3 = nn.Conv2d(16, 32, 8)
+        self.fc1 = nn.Linear(32 * 25 * 25, output_dim)
         self.save_hyperparameters()
         self.accuracy = torchmetrics.Accuracy()
 
     def forward(self, x):
-        x = self.flatten(x)
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = self.pool(F.relu(self.conv3(x)))
+        x = torch.flatten(x, 1) # flatten all dimensions except batch
         x = self.fc1(x)
+        x = F.softmax(x, dim=1)
         return x
 
     def training_step(self, batch, batch_idx):
         # training_step defined the train loop.
         # It is independent of forward
         x, y = batch
-        x = self.forward(x)
-        y_hat = F.softmax(x, dim=1)
+        y_hat = self(x)
         loss = F.cross_entropy(y_hat, y)
         # Logging to TensorBoard by default
         self.log('train_loss', loss)
         self.log('train_acc', self.accuracy(y_hat, y))
         return loss
 
+    def validation_step(self, batch, batch_idx):
+        x, y = batch
+        y_hat = self(x)
+        loss = F.cross_entropy(y_hat, y)
+        self.log('val_loss', loss)
+        self.log('val_acc', self.accuracy(y_hat, y))
+        return loss
+
     def configure_optimizers(self):
-        optimizer = torch.optim.Adam(self.parameters(), lr=1e-3)
+        optimizer = torch.optim.Adam(self.parameters(), lr=1e-4)
         return optimizer
--- try_sida.py
+++ try_sida.py
@@ -1,31 +1,39 @@
+#%%
 from typing import Any, Callable, cast, Dict, List, Optional, Tuple
+import os
 import pandas as pd
+
 import torch
+from torch import nn
+import torch.nn.functional as F
 import torchvision
+from torchvision import transforms
+from torchvision.datasets import MNIST
+from torch.utils.data import DataLoader, random_split
 import pytorch_lightning as pl
-from models.pl_image_classifier import LitImageClassifier
 from pytorch_lightning.loggers import WandbLogger
 logger = WandbLogger(project="face")
+from models.pl_image_classifier import LitImageClassifier
 
-# read labels
+#%%
 df_labels = pd.read_csv("./data-labels/ffhq_aging_labels.csv")
+# keep only confident ones (20778 rows)
 df_labels = df_labels[df_labels['age_group_confidence']>0.9]
+df_labels = df_labels[df_labels['gender_confidence']>0.9]
+# get targets
 labels_age_group = df_labels['age_group'].unique().tolist()
 targets = {}
 for i,l in enumerate(labels_age_group):
     targets[l] = i
-targets
+targets # {'0-2':0, ...}
 
-# divide into two subsets by gender
+#%%
+#%%
 df_male = df_labels[df_labels['gender']=='male']
 df_female = df_labels[df_labels['gender']=='female']
 
-# dataset
+#%%
 class Subset:
-    """
-        prepare the dataset for DataLoader
-        hard code for now
-    """
     def __init__(self, df: pd.DataFrame) -> None:
         self.df = df
 
@@ -43,16 +51,30 @@ class Subset:
         return len(self.df)
 
 set_male = Subset(df_male)
+l = len(set_male)
+trainset_male, valset_male = torch.utils.data.random_split(set_male, [l-1000, 1000])
 
-# dataloader
+for i, (data, target) in enumerate(trainset_male):
+    print(i,data.shape,target)
+    if i>10: break
+#%%
 train_loader = torch.utils.data.DataLoader(
-    set_male,
-    batch_size=64,
+    trainset_male,
+    batch_size=128,
     num_workers=6,
     shuffle=True
 )
-
-# train
+val_loader = torch.utils.data.DataLoader(
+    valset_male,
+    batch_size=128,
+    num_workers=6,
+    shuffle=True
+)
+# for batch_idx, (data, target) in enumerate(train_loader):
+#     print(target)
+#     break
+# %%
 model = LitImageClassifier()
-trainer = pl.Trainer(gpus=1, logger=logger, max_epochs=3)
-trainer.fit(model, train_loader)
+trainer = pl.Trainer(gpus=1, logger=logger, max_epochs=20, val_check_interval=5)
+trainer.fit(model, train_dataloader=train_loader, val_dataloaders=val_loader)
+# %%
